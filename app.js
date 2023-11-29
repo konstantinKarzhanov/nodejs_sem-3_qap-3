64cfe593412e32f47e3135abd2183c557c84db7d
kostiantyn <kostiantyn@test.com>
2023-11-29 16:14:15 -0330

    +app.js main application file

---
+++ app.js
@@ -0,0 +1,26 @@
+// Import external packages
+require("dotenv").config();
+const express = require("express");
+
+// Import routes from custom modules
+const indexRouter = require("./routes/indexRouter");
+const apiRouter = require("./routes/apiRouter");
+const guestsRouter = require("./routes/guestsRouter");
+
+// Set app
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+
+// Set middleware
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+// Set routes
+app.use("/", indexRouter);
+app.use("/api", apiRouter);
+app.use("/guests", guestsRouter);
+
+// Start the server
+app.listen(PORT, console.log(`Server is listening on port: ${PORT}`));
