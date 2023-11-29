a766a3dd5a3303914d3813f8ee6070deb633333f
kostiantyn <kostiantyn@test.com>
2023-11-29 16:16:51 -0330

    +config files for the application

---
+++ config/pgPool.js
@@ -0,0 +1,13 @@
+// Import external packages
+const { Pool } = require("pg");
+
+// Set pool
+const pool = new Pool({
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT,
+});
+
+module.exports = pool;
