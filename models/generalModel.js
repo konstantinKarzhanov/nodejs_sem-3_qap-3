8e8eea443a5f8de35a28aa58a4adecc5bcb319c4
kostiantyn <kostiantyn@test.com>
2023-11-29 16:55:00 -0330

    +data access layer

---
+++ models/generalModel.js
@@ -0,0 +1,49 @@
+// Import required functions/variables from custom modules
+const pool = require("../config/pgPool");
+
+// Define function to get data from specified table
+const getAllData = async (table, orderByColumn, orderByOrder, limit) => {
+  try {
+    const query = `SELECT * FROM ${table} ORDER BY ${orderByColumn} ${orderByOrder} LIMIT ${limit}`;
+    const result = await pool.query(query);
+
+    return result.rows;
+  } catch (err) {
+    console.log(err.message);
+    throw err;
+  }
+};
+
+// Define function to get data from specified table using id
+const getDataByID = async (table, idColumn, id) => {
+  try {
+    const query = `SELECT * FROM ${table} WHERE ${idColumn} = $1`;
+    const result = await pool.query(query, [id]);
+
+    return result.rows;
+  } catch (err) {
+    console.log(err.message);
+    throw err;
+  }
+};
+
+// Define function to add new data to the database
+const addData = async (table, keyArr, valueArr) => {
+  try {
+    const query = `INSERT INTO ${table}(${keyArr}) VALUES (${valueArr.map(
+      (_, index) => "$" + (index + 1)
+    )})`;
+
+    await pool.query(query, [...valueArr]);
+  } catch (err) {
+    console.log(err.message);
+    throw err;
+  }
+};
+
+// Export functions/variables to use in other modules
+module.exports = {
+  getAllData,
+  getDataByID,
+  addData,
+};
