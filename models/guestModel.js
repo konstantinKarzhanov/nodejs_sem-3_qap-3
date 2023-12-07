8faccbeea9c9679284331638956e665c590c3908
kostiantyn <kostiantyn@test.com>
2023-12-07 10:51:50 -0330

    +updateGuestAddress, +updateGuest, +patch route
    Also wrapped 'deleteGuestNullReservation' in the try...catch function

--- models/guestModel.js
+++ models/guestModel.js
@@ -53,6 +53,55 @@ const createGuestAddress = async ({
   }
 };
 
+// Define a function to update a record in the "guest" table and update/create in the "address" table
+const updateGuestAddress = async ({
+  guest_id,
+  guest_fname,
+  guest_lname,
+  guest_dob,
+  guest_email,
+  guest_phone,
+  street,
+  city,
+  province,
+  postal_code,
+}) => {
+  // Set a variable that is part of the final query
+  const insertIntoAddressQuery =
+    "INSERT INTO address (street, city, province, postal_code) VALUES ($1, $2, $3, $4)";
+
+  // Set the parts of the COALESCE function
+  const selectAddressIdFromCTEQuery = "SELECT address_id FROM cte_address";
+  const selectAddressIdFromAddressQuery =
+    "SELECT address_id FROM address WHERE street = $1 AND city = $2 AND province = $3 AND postal_code = $4)";
+  const coalesceAddressIdQuery = `COALESCE((${selectAddressIdFromCTEQuery}), (${selectAddressIdFromAddressQuery})`;
+
+  // Set variables that are the main blocks of the final query
+  const cte = `WITH cte_address AS (${insertIntoAddressQuery} ON CONFLICT ON CONSTRAINT uq_address DO NOTHING RETURNING address_id)`;
+  const setClause = `SET address_id = ${coalesceAddressIdQuery}, guest_fname = $5, guest_lname = $6, guest_dob = $7, guest_email = $8, guest_phone = $9`;
+
+  // Set the query variable
+  const query = `${cte} UPDATE guest ${setClause} WHERE guest_id = $10;`;
+
+  try {
+    await pool.query(query, [
+      street,
+      city,
+      province,
+      postal_code,
+      guest_fname,
+      guest_lname,
+      guest_dob,
+      guest_email,
+      guest_phone,
+      guest_id,
+    ]);
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
+
 // Define a function to delete a guest from the database and set the corresponding guest_id to NULL in the 'reservation' table
 const deleteGuestNullReservation = async (keyArr, valueArr) => {
   const conditionArr = keyArr.map((item, index) => `${item} = $${index + 1}`);
@@ -84,5 +133,6 @@ const deleteGuestNullReservation = async (keyArr, valueArr) => {
 
 module.exports = {
   createGuestAddress,
+  updateGuestAddress,
   deleteGuestNullReservation,
 };
