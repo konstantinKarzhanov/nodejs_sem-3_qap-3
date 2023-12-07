b19f962b7d4045514b7c3d80d462604fe8cdb57c
kostiantyn <kostiantyn@test.com>
2023-12-07 13:12:17 -0330

    Separated 'guest.js' into 'guest.js' and 'guestUtils.js' to use as modules

--- public/scripts/guests.js
+++ public/scripts/guests.js
@@ -1,74 +1,30 @@
+// Import required functions/variables from custom modules
+import {
+  setValueOfHiddenControl,
+  setControlValue,
+  setInitialBtnState,
+  setBtnState,
+  confirmSubmit,
+} from "./guestsUtils.js";
+
 // Define required variables
 const list = document.querySelector("#list--guests");
 const form = document.forms["form--guests"];
-const formDataInputArr = Array.from(
+const formDataControlArr = Array.from(
   new FormData(form).keys(),
   (item) => form[item]
 );
-const formDataInputSubArr = formDataInputArr.reduce((acc, current) => {
+const formDataControlSubArr = formDataControlArr.reduce((acc, current) => {
   if (current.name.includes("guest_")) {
     acc.push(current);
   }
   return acc;
 }, []);
-const {
-  "btn--reset": btnReset,
-  "btn--submit": btnSubmit,
-  "btn--delete": btnDelete,
-} = form;
 
-// Define a function to set initial state of the buttons
-const setControlsToInitialState = () => {
-  formDataInputArr.forEach(
-    (item) => (item.value = item.type == "hidden" ? "" : item.value)
-  );
-  btnSubmit.textContent = "Add";
-
-  btnReset.disabled = true;
-  btnSubmit.disabled = true;
-  btnDelete.disabled = true;
-};
-
-// Define a function to set state of the buttons based on the inputs value
-const setBtnState = (arr, subArr) => {
-  if (arr.every((item) => item.value.trim() != "")) {
-    btnReset.disabled = false;
-    btnSubmit.disabled = false;
-    btnDelete.disabled = false;
-  } else if (arr.every((item) => item.value.trim() == "")) {
-    setControlsToInitialState();
-  } else if (subArr.some((item) => item.value.trim() != "")) {
-    btnReset.disabled = false;
-    btnSubmit.disabled = true;
-    btnDelete.disabled = false;
-  } else {
-    btnReset.disabled = false;
-    btnSubmit.disabled = true;
-    btnDelete.disabled = true;
-  }
-};
-
-// Define a function to set inputs value using the target element's id and text content
-const setInputValue = (target, arr) => {
-  const { id, textContent } = target;
-  const targetArr = [id.split("-").at(-1), ...textContent.split(", ")];
-
-  arr.forEach((item, index) => {
-    item.value = targetArr[index];
-  });
-};
-
-// Define a function to confirm submittion
-const confirmSubmit = (arr, message) => {
-  const data = arr
-    .map(({ value, parentElement }) => {
-      if (value.trim() != "")
-        return `${parentElement.textContent.trim()}: "${value}"\n`;
-    })
-    .join("");
-
-  return confirm(`${message}:\n\n${data}`);
-};
+const btnObjArr = [...form.elements].reduce((acc, current) => {
+  if (current.tagName == "BUTTON") acc.push(current);
+  return acc;
+}, []);
 
 // Define a function to handle "click" event
 const handleClick = (event) => {
@@ -76,18 +32,24 @@ const handleClick = (event) => {
 
   if (!target) return;
 
-  setInputValue(target, formDataInputArr);
-  btnSubmit.textContent = "Update";
+  setControlValue(target, formDataControlArr);
 
-  setBtnState(formDataInputArr.slice(1), formDataInputSubArr.slice(1));
+  btnObjArr.forEach((item) => {
+    if (item.id == "btn--post") item.textContent = "Update";
+  });
+
+  setBtnState(formDataControlArr, formDataControlSubArr, btnObjArr);
 };
 
 // Define a function to handle "input" event
 const handleInput = () =>
-  setBtnState(formDataInputArr.slice(1), formDataInputSubArr.slice(1));
+  setBtnState(formDataControlArr, formDataControlSubArr, btnObjArr);
 
 // Define a function to handle "reset" event
-const handleReset = () => setControlsToInitialState();
+const handleReset = () => {
+  setValueOfHiddenControl(formDataControlArr);
+  setInitialBtnState(btnObjArr);
+};
 
 // Define a function to handle "submit" event
 function handleSubmit(event) {
@@ -101,10 +63,10 @@ function handleSubmit(event) {
   if (text == "delete") {
     message =
       "Click 'OK' if you want to delete ALL records which have data like";
-    isConfirmed = confirmSubmit(formDataInputSubArr.slice(1), message);
+    isConfirmed = confirmSubmit(formDataControlSubArr.slice(1), message);
   } else if (text == "update") {
     message = "Click 'OK' if you want to update record with new data";
-    isConfirmed = confirmSubmit(formDataInputArr.slice(1), message);
+    isConfirmed = confirmSubmit(formDataControlArr.slice(1), message);
   }
 
   if (!isConfirmed) return;
---
+++ public/scripts/guestsUtils.js
@@ -0,0 +1,72 @@
+// Define a function to set value of the "hidden" controls
+const setValueOfHiddenControl = (controlArr) => {
+  controlArr.forEach(
+    (item) => (item.value = item.type == "hidden" ? "" : item.value)
+  );
+};
+
+// Define a function to set controls value using the target element's id and text content
+const setControlValue = (target, controlArr) => {
+  const { id, textContent } = target;
+  const targetArr = [id.split("-").at(-1), ...textContent.split(", ")];
+
+  controlArr.forEach((item, index) => {
+    item.value = targetArr[index];
+  });
+};
+
+// Define a function to set initial state of the buttons
+const setInitialBtnState = (btnArr) => {
+  btnArr.forEach((item) => {
+    if (item.id == "btn--post") item.textContent = "Add";
+
+    item.disabled = true;
+  });
+};
+
+// Define a function to set state of the buttons based on the controls value
+const setBtnState = (controlArr, controlSubArr, btnArr) => {
+  if (controlArr.slice(1).every((item) => item.value.trim() != "")) {
+    btnArr.forEach((item) => (item.disabled = false));
+  } else if (controlArr.slice(1).every((item) => item.value.trim() == "")) {
+    setValueOfHiddenControl(controlArr);
+    setInitialBtnState(btnArr);
+  } else if (controlSubArr.slice(1).some((item) => item.value.trim() != "")) {
+    btnArr.forEach((item) => {
+      if (item.id == "btn--post") {
+        item.disabled = true;
+      } else {
+        item.disabled = false;
+      }
+    });
+  } else {
+    btnArr.forEach((item) => {
+      if (item.id == "btn--post" || item.id == "btn--delete") {
+        item.disabled = true;
+      } else {
+        item.disabled = false;
+      }
+    });
+  }
+};
+
+// Define a function to confirm submittion
+const confirmSubmit = (controlArr, message) => {
+  const data = controlArr
+    .map(({ value, parentElement }) => {
+      if (value.trim() != "")
+        return `${parentElement.textContent.trim()}: "${value}"\n`;
+    })
+    .join("");
+
+  return confirm(`${message}:\n\n${data}`);
+};
+
+// Export functions/variables to use in other modules
+export {
+  setValueOfHiddenControl,
+  setControlValue,
+  setInitialBtnState,
+  setBtnState,
+  confirmSubmit,
+};
