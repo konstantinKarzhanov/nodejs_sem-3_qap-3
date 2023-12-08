dea7ba890b9ea6bb6fba4efb0f74d70e4eeeffec
kostiantyn <kostiantyn@test.com>
2023-12-07 23:15:30 -0330

    +express.json()

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.set("view engine", "ejs");
 // Set middleware
 app.use(express.static("public"));
 app.use(methodOverride("_method"));
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Set routes
