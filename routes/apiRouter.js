899bdd1bf1e67b1a5e2af3f5e95a8c1ebf51b673
kostiantyn <kostiantyn@test.com>
2023-12-07 23:18:39 -0330

    +controller functions and route handlers for api

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -2,22 +2,47 @@
 const express = require("express");
 
 // Import required functions/variables from custom modules
-const { getGuests, getGuestById } = require("../controllers/guestsController");
+const { navbarApiMap } = require("../config/defaults");
+const {
+  getGuests,
+  getGuestById,
+  postGuest,
+  updateGuest,
+  deleteGuest,
+} = require("../controllers/guestsController");
 
 // Set router
 const apiRouter = express.Router();
 
 // Set route handlers
 apiRouter.get("/", (req, res) => {
-  res.send("API Page");
+  res.render("api", {
+    title: "API",
+    h1Title: "API Page",
+    navbar: navbarApiMap,
+  });
 });
 
-apiRouter.get("/guests", getGuests, (req, res) => {
-  res.send(res.data);
-});
+apiRouter
+  .route("/guests")
+  .get(getGuests, (req, res) => {
+    res.json(res.data);
+  })
+  .post(postGuest, (req, res) => {
+    res.redirect("api/guests");
+  })
+  .delete(deleteGuest, (req, res) => {
+    res.redirect("api/guests");
+  });
+// .patch(updateGuest, (req, res) => {
+//   res.redirect("api/guests");
+// })
+// .put(updateGuest, (req, res) => {
+//   res.redirect("api/guests");
+// })
 
 apiRouter.get("/guests/:id", getGuestById, (req, res) => {
-  res.send(res.data);
+  res.json(res.data);
 });
 
 // Export the router to use in other modules
